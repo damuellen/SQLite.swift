6ce36e0a536d90566a59b26e87a45b45d51d17be
Daniel Müllenborn <damuellen@aol.com>
2020-10-20 22:31:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 )
 
 #if os(Linux)
-    package.dependencies = [.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")]
+    package.dependencies = [.package(name: "CSQLite", url: "https://github.com/damuellen/CSQLite.git", from: "1.0.0")]
     package.targets = [
         .target(name: "SQLite", exclude: ["Extensions/FTS4.swift", "Extensions/FTS5.swift"]),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests", exclude: [
